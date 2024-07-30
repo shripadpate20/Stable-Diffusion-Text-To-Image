43e30345de9a3d222fd0cec67e8dbbb9bedac6c9
M23MAC007 <m23mac007@iitj.ac.in>
2024-07-31 00:27:43 +0530

    add residualblock in decoder.py file

--- decoder.py
+++ decoder.py
@@ -25,5 +25,34 @@ class VAE_attentionBlock(nn.Module):
         return x
     
 
+class VAE_ResidualBlock(nn.Module):
+    def __init__(self,in_channels,out_channels):
+        super().__init__()
+        self.groupnorm_1 = nn.GroupNorm(32,in_channels)
+        self.conv_1 = nn.conv2d(in_channels,out_channels,kernel_size = 3,padding=1)
+
+        self.groupnorm_2 = nn.GroupNorm(32,in_channels)
+        self.conv_2 = nn.Conv2d(in_channels,out_channels,kernel_size=3,padding=1)
+
+        if in_channels ==out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels,out_channels,kernel_size=1,padding=0)
+
+
+    def forward(self,x):
+        residual = x
+        
+        x = self.groupnorm_1(x)
+        x = F.silu(x)
+        x = self.conv_1(x)
+        x = self.groupnorm_2(x)
+        x = F.silu(x)
+        x = self.conv_2(x)
+
+        return x + self.residual_layer(residual)
+    
+
+    
 
 
